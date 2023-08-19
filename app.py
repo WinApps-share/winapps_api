5732e1ab1ce678af5269f2dd3966f01addfa15cf
ayong <007ayongsvip@gmail.com>
2023-08-19 12:12:22 +0800

    跨域

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from info_routes import info_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # 注册蓝图
 app.register_blueprint(info_bp)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
